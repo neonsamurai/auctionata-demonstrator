ef2da4cdeaf1e1b7b9a2a24b36f4cf88de7f6721
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2016-10-03 00:00:49 +0200

    Add spec for auction rendering an item.

--- imports/ui/AuctionComponent.spec.js
+++ imports/ui/AuctionComponent.spec.js
@@ -3,13 +3,28 @@ import { Factory } from 'meteor/dburles:factory';
 import React from 'react';
 import { shallow } from 'enzyme';
 import AuctionComponent from './AuctionComponent.jsx';
+import ItemComponent from './ItemComponent.jsx';
+import Items from '../api/item/itemModel.js';
+
+Factory.define('item', Items, {
+  name: 'Pretty Watch',
+  description: 'This is a shiny watch with lots of bling.',
+  price: 10 * 1000
+});
 
 describe('Auction component', function(){
+  const PRICE = 100;
+  const auctionItem = Factory.build('item',{price: PRICE});
+  const item = shallow(<AuctionComponent item={auctionItem}/>);
+
   it('should by default render with current price = item price, highest bidder empty', function(){
-    const PRICE = 100;
-    const auction = Factory.build('auction',{price: PRICE});
-    const item = shallow(<AuctionComponent auction={item} />);
-    expect(item.find('#currentPrice').text).to.equal(PRICE);
-    expect(item.find('#highestBidder').text).to.be.empty;
+
+
+
+    expect(item.find('#currentPrice').text()).to.equal(PRICE.toString());
+    expect(item.find('#highestBidder').text()).to.be.empty;
+  });
+  it('should render an Item component', function(){
+    expect(item.find(ItemComponent).length).to.equal(1);
   });
 });
