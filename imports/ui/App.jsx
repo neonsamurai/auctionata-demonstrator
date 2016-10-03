8e132cd0eb4ecc16c71a83228fd225265f363a8c
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2016-10-03 02:29:19 +0200

    Add data container.

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import AccountsUiComponent from './AccountsUiComponent.jsx'
+import AuctionComponent from './AuctionComponent.jsx';
+import { Auctions } from '../api/auction/auctionModel.js';
+import { Items } from '../api/item/itemModel.js';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default App = () => {
-    return (
-        <div>
-            <p>Hello World!</p>
-        </div>
-    );
+class App extends Component {
+    const item = Items.findOne();
+    const auction = Auctions.findOne();
+
+    renderAuction = () => <AuctionComponent item={this.props.item} highestBidder={this.props.auction.highestBidder}/>;
+    renderLoginNotification = () => <div>Please login ...</div>;
+
+    render() {
+        return (
+            <div>
+                <h1>Auctionata demonstrator</h1>
+                <AccountsUiComponent />
+                {Meteor.userId() ? renderAuction() : renderLoginNotification()}
+            </div>
+        );
+    }
 }
+
+App.propTypes = {
+    item: PropTypes.object.isRequired,
+    auction: PropTypes.object.isRequired,
+}
+
+export default createContainer(() => {
+    return {
+        item: Item.findOne(),
+        auction: Auctions.findOne(),
+    };
+}, App);
