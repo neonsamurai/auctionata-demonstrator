ae220dd88f1aeee6e63f18f5a5889ece22393a0c
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2016-10-03 12:02:15 +0200

    Remove debug logging.

--- imports/api/auction/auctionMethods.js
+++ imports/api/auction/auctionMethods.js
@@ -10,24 +10,28 @@ Meteor.methods({
   /**
    * Place a bid from a user.
    * @param auction Auctions instance.
-   * @param userId ID of the user who placed the bid.
    */
   'bid.place': function (auction) {
     check(auction, Object);
+    // Check if user is authorized / logged in
     if(! this.userId){
       throw new Meteor.Error('not authorized!');
     };
     const currentAuction = Auctions.findOne(auction._id);
     if (currentAuction.highestBidder.length === 0) {
-      console.log(currentAuction);
       const mutator = { $set: { highestBidder: Meteor.user().emails[0].address}};
       const id = currentAuction._id;
       Auctions.update(currentAuction._id, mutator)
     }
     return;
   },
+  /**
+   * Resets the auction's highest bidder. Only for demonstration purposes.
+   * @param auction
+   */
   'bid.reset': function (auction) {
     check(auction, Object);
+    // Check if user is authorized / logged in
     if(this.userId){
       const mutator = { $set: { highestBidder: ''}};
       Auctions.update(auction._id, mutator);
