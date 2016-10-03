c0ff38209e2087a241ee92d15a09dd2a36a558fc
Tim Jagodzinski <tim.jagodzinski@gmail.com>
2016-10-03 11:08:06 +0200

    Add input validation.

--- imports/api/auction/auctionMethods.js
+++ imports/api/auction/auctionMethods.js
@@ -1,24 +1,38 @@
 import { Meteor } from 'meteor/meteor';
 import { Auctions } from './auctionModel.js'
 
+/**
+ * @module api/auction/auctionMethods
+ * Defines Meteor methods related to the auction business logic.
+ */
+
 Meteor.methods({
-  'bid.place': function (auction, userId) {
-    console.log(auction);
+  /**
+   * Place a bid from a user.
+   * @param auction Auctions instance.
+   * @param userId ID of the user who placed the bid.
+   */
+  'bid.place': function (auction) {
+    check(auction, Object);
+    if(! this.userId){
+      throw new Meteor.Error('not authorized!');
+    };
     const currentAuction = Auctions.findOne(auction._id);
     if (currentAuction.highestBidder.length === 0) {
       console.log(currentAuction);
       const mutator = { $set: { highestBidder: Meteor.user().emails[0].address}};
       const id = currentAuction._id;
-      console.log(mutator);
-      console.log(id);
       Auctions.update(currentAuction._id, mutator)
     }
     return;
   },
-  'bid.reset': function (auction, userId) {
-    if(Meteor.userId){
+  'bid.reset': function (auction) {
+    check(auction, Object);
+    if(this.userId){
       const mutator = { $set: { highestBidder: ''}};
       Auctions.update(auction._id, mutator);
+    } else {
+      throw new Meteor.Error('not authorized!');
     }
   }
 })
